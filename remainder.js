4170884ae11f57cf1a0e77e23f771ba5f38a2249
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-28 10:38:13 +0530

    Create remainder.js

---
+++ remainder.js
@@ -0,0 +1,11 @@
+//let output = remainder(numbers);
+//let numbers = [2,4];
+//console.log(output);
+
+function remainder(a,b){
+    let c =  a % b;
+    console.log(c);  
+    return c;  
+}
+
+remainder(2,4);
