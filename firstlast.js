a63788e3da5b19670ce52a305a5c77249d6debde
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-28 10:30:30 +0530

    Create firstlast.js

---
+++ firstlast.js
@@ -0,0 +1,14 @@
+let numbers = [1,3,4,7,8,2,10];
+
+function firstNo(numbers){
+    let i = 0;
+    console.log(numbers[i]);
+}
+
+function LastNo(numbers){
+    let i = numbers.length-1;
+    console.log(numbers[i]);
+}
+
+firstNo(numbers);
+LastNo(numbers);
