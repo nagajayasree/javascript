269d28b2e90118244d120e298515ca57b53218c0
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-28 17:56:54 +0530

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,12 @@
+function sum(a,b){
+    if(a != b){
+        let c = a + b;
+        console.log(c);
+    }
+    else if(a === b){
+        sum = a * 3;
+        console.log(sum);
+    }
+}
+
+sum(1,5);
