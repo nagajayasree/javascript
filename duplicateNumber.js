9dd1240c396c318f30618f2aa167db2acd16762b
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-30 12:56:52 +0530

    Update duplicateNumber.js

--- duplicateNumber.js
+++ duplicateNumber.js
@@ -1,8 +1,9 @@
-const numbers1 = [1,1,3,2,4,5];
+
+const numbers1 = [1,2,2,4];
 const numbers2 = [];
 let i = 0;
 let j = 0;
-while(i <= numbers1.length){
+while(i <= numbers1.length-1){
     if(numbers1.indexOf(i) != numbers1.lastIndexOf(i)){
         console.log(numbers1);
         console.log("duplicate number exists");
