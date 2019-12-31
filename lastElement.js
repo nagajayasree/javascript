708219841ba6eee774747c5364888b27d7e95cbd
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-31 10:54:46 +0530

    Create lastElement.js

---
+++ lastElement.js
@@ -0,0 +1,10 @@
+//create function that takes array
+//and returns last element.
+
+let array = [1,2,5,8,3];
+let output = lastElement(array);
+console.log(output);
+
+function lastElement(array){
+    return array[array.length-1];
+}
