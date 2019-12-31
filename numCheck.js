a5bfbf57c89a4d331d0591c1470293027fb137d0
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-31 11:41:32 +0530

    Create numCheck.js

---
+++ numCheck.js
@@ -0,0 +1,9 @@
+//write a function to check if array
+//contains particular number
+
+let array = [1,2,3,4,5];
+let output = numCheck(array,3);
+
+function numCheck(array,element){
+    console.log(array.includes(element));
+}
