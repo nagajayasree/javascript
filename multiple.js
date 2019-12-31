3876b868091478f7c668b41f4d604c7c51f8a7fb
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-31 10:27:13 +0530

    Create multiple.js

---
+++ multiple.js
@@ -0,0 +1,9 @@
+//create function that takes integer
+//and returns true if it is divisible by 100 otherwise false
+
+function multiple(n){
+    let result = (n % 100 == 0) ? true : false;
+    console.log(result);
+}
+
+multiple(1000);
