fb4b22ac1540df6aedfddf1fa7cd1c7c8af4974b
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2019-12-31 10:17:58 +0530

    Create equalityCheck.js

---
+++ equalityCheck.js
@@ -0,0 +1,9 @@
+//implement function that true if type of parameters are
+//equal,false otherwise
+
+function equalityCheck(a,b){
+    let result = (typeof(a)=== typeof(b)) ? true : false;
+    console.log(result);
+}
+
+equalityCheck(1,true);
