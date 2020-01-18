aaee8406920d04437af66be9184743badfbeb738
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-01-18 09:42:48 +0530

    Create num15.js

---
+++ num15.js
@@ -0,0 +1,9 @@
+Write a JavaScript program to test whether a given integer is greater than 15 return the given number, otherwise return 15.
+
+let num15 = function(n){
+    if(n >= 15) return n;
+    return 15;
+}
+
+let output = num15(20);
+console.log(output);
