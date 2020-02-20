e7902591ae01f28de43f1cb9c662021644082980
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-02-21 00:25:17 +0530

    Create addNew.js

---
+++ addNew.js
@@ -0,0 +1,19 @@
+//program to create a new string adding "New!" in front of a given string.
+//If given string begins with "New!" already then return original string.
+
+function addNew(string){
+    if(string.slice(0,4) == "New!")
+    {
+        return "Original String";
+    }
+    else{
+        let newString = "New!"+" "+`${string}`;
+        return newString;
+    }
+}
+
+let result = addNew("New! Mobile");
+console.log(result);
+
+let output = addNew("Car");
+console.log(output);
