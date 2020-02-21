9c832c4229f8e563fd0f69d6d8276e9d312d8492
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-02-21 13:41:20 +0530

    Update halfString.js

--- halfString.js
+++ halfString.js
@@ -1,8 +1,7 @@
 //program to extract first half of string of even length
 
 function halfString(input){
-    let str = (input.length)/2;
-    console.log(input.slice(0,str));
+    console.log(input.slice(0,input.length/2));
 }
 
 halfString("jayasree");
