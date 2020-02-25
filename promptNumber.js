b601ae0f565819accc9a46c28b9f6ea0ba8f649c
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-02-26 02:42:32 +0530

    Create promptNumber.js

---
+++ promptNumber.js
@@ -0,0 +1,12 @@
+//Using if..else, write the code which gets a number via prompt and then shows in alert:
+//1, if the value is greater than zero,
+//-1, if less than zero,
+//0, if equals zero.
+
+
+let number = prompt("Number",'');
+if(number == 0){
+    alert('0');
+}else if(number > 0){
+    alert('1');
+}else alert('-1');
