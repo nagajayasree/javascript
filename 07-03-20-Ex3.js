0cc7ab9576d7b800e01d5b4fc8c995b18a950fdf
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-03-07 01:13:26 +0530

    Create 07-03-20-Ex3.js

---
+++ 07-03-20-Ex3.js
@@ -0,0 +1,6 @@
+//Write a JavaScript program to get the current date. 
+let date = new Date();
+console.log((date.getMonth()+1)+"-"+date.getDate()+"-"+date.getFullYear());
+console.log((date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear());
+console.log(date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear());
+console.log(date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear());
